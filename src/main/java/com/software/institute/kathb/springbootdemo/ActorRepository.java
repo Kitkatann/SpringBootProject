54a791a462aad2a28d8e072aa26c6b1a88571b24
Kitkatann <kath.boyfield@hotmail.co.uk>
2022-07-26 15:34:55 +0100

    Connected to the Sakila database

---
+++ src/main/java/com/software/institute/kathb/springbootdemo/ActorRepository.java
@@ -0,0 +1,7 @@
+package com.software.institute.kathb.springbootdemo;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ActorRepository extends CrudRepository<Actor,Integer> {
+
+}
--- src/main/java/com/software/institute/kathb/springbootdemo/SpringbootdemoApplication.java
+++ src/main/java/com/software/institute/kathb/springbootdemo/SpringbootdemoApplication.java
@@ -1,25 +1,48 @@
 package com.software.institute.kathb.springbootdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins="*")
 @SpringBootApplication
 @RestController
+@RequestMapping("/Sakila")
 public class SpringbootdemoApplication {
+
+	@Autowired
+	private ActorRepository actorRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootdemoApplication.class, args);
 	}
 
-	@CrossOrigin("http://localhost:3000")
-	@GetMapping("/customRoute")
-	public String MyResponse()
-	{
-		Inventory inventory = new Inventory();
-		return inventory.toString();
+	public SpringbootdemoApplication(ActorRepository actorRepository){
+		this.actorRepository = actorRepository;
+	}
+
+
+	//@ResponseBody
+	@PostMapping("/Add_New_Actor")
+	public @ResponseBody String addNewActor(@RequestParam String first_name, @RequestParam String last_name){
+		Actor a = new Actor(first_name, last_name);
+		System.out.println(first_name + " " + last_name);
+		actorRepository.save(a);
+		return "saved";
+	}
+
+	//Get request / read function
+	@GetMapping("/All_Actors")
+	public @ResponseBody
+	Iterable<Actor>getAllActors(){
+		return actorRepository.findAll();
+	}
+
+	@DeleteMapping("/Delete_Actor_By_Id")
+	public @ResponseBody
+	void deleteActorById(@RequestParam int id){
+		actorRepository.deleteById(id);
 	}
 
 }
